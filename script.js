b24dc74f4f316385830f67bba144637870513c04
rammerhq <126903975+rammerhq@users.noreply.github.com>
2023-04-20 15:09:23 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/https://www.youtube.com/watch?v=dQw4w9WgXcQ
+//https://www.youtube.com/watch?v=dQw4w9WgXcQ
 //https://www.youtube-nocookie.com/embed/dQw4w9WgXcQ?wmode=transparent&amp;iv_load_policy=3&amp;autoplay=1&amp;html5=1&amp;showinfo=0&amp;rel=0&amp;modestbranding=1&amp;playsinline=0&amp;theme=light
 const base = "https://www.youtube-nocookie.com/embed/";
 const end = "?wmode=transparent&amp;iv_load_policy=3&amp;autoplay=0&amp;html5=1&amp;showinfo=0&amp;rel=0&amp;modestbranding=1&amp;playsinline=0&amp;theme=light";
